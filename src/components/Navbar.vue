e909403e46609b0a8b8a8b594b5a781ebbd08c4e
xelinel32 <artemsedlyar99@gmail.com>
2020-08-13 14:34:39 +0300

    init

---
+++ src/components/Navbar.vue
@@ -0,0 +1,101 @@
+<template>
+  <nav>
+    <v-navigation-drawer v-model="drawer" dark app class="blue accent-2">
+      <v-row class="flex-column">
+        <v-col class="mt-10 mb-10" align="center">
+          <v-avatar>
+            <v-icon large>fas fa-store</v-icon>
+          </v-avatar>
+        </v-col>
+      </v-row>
+      <v-list>
+        <v-list-item
+          v-for="link in links"
+          :key="link.text"
+          router
+          :to="link.route"
+          active-class="border"
+        >
+          <v-list-item-action>
+            <v-icon>{{ link.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ link.text }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <v-row class="ma-2">
+        <template>
+          <v-carousel
+            cycle
+            dark
+            height="230"
+            hide-delimiter-background
+            show-arrows-on-hover
+          >
+            <v-carousel-item v-for="(slide, i) in slides" :key="i">
+              <v-sheet color="white" height="80%" align="center" tile>
+                <v-row class="fill-height" align="center" justify="center">
+                  <div class="grey--text">
+                    <h4>Traffic fines</h4>
+                    <p class="ma-2">{{ slide }}</p>
+                    <v-btn outlined color="indigo">Read more</v-btn>
+                  </div>
+                </v-row>
+              </v-sheet>
+            </v-carousel-item>
+          </v-carousel>
+        </template>
+      </v-row>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: true,
+      links: [
+        {
+          icon: 'fas fa-home',
+          text: 'Home',
+          route: '/',
+        },
+        {
+          icon: 'far fa-credit-card',
+          text: 'Balance',
+          route: '/balance',
+        },
+        {
+          icon: 'fas fa-chart-line',
+          text: 'Investments',
+          route: '/investments',
+        },
+        {
+          icon: 'fas fa-balance-scale-right',
+          text: 'Insurance',
+          route: '/insurance',
+        },
+        {
+          icon: 'fas fa-briefcase',
+          text: 'Buisness',
+          route: '/buisness',
+        },
+      ],
+      slides: [
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+      ],
+    }),
+  }
+</script>
+
+<style scoped>
+  .border {
+    border-left: 4px solid #2c3e50;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,105 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app :style="{ background: $vuetify.theme.themes.light.background }">
+    <v-container>
+      <v-row class="ma-4">
+        <v-app style="background-color: grey lighten-1;" class="rounded">
+          <v-row>
+            <v-col cols="12" md="8" class="ligne">
+              <v-container>
+                <v-row>
+                  <v-col cols="12" md="9" offset-md="1">
+                    <v-text-field label="Search" prepend-inner-icon="search">
+                    </v-text-field>
+                  </v-col>
+                  <v-col cols="12" md="2" align="center">
+                    <v-badge color="error" overlap class="mt-5">
+                      <template v-slot:badge>2</template>
+                      <v-icon color="tertiary">
+                        mdi-bell
+                      </v-icon>
+                    </v-badge>
+                  </v-col>
+                  <v-col cols="12" md="12">
+                    <h1 class="headline mb-2 grey--text">Money Status</h1>
+                    <template>
+                      <div id="app">
+                        <GChart
+                          type="ColumnChart"
+                          :data="chartData"
+                          :options="chartOptions"
+                        ></GChart>
+                      </div>
+                    </template>
+                  </v-col>
+                  <v-col cols="12" md="12">
+                    <h1 class="headline mb-2 grey--text">Budgets</h1>
+                  </v-col>
+                  <v-col cols="12" md="8">
+                    <v-card>
+                      <v-list-item>
+                        <v-list-item-avatar color="blue">
+                          <v-icon dark>fas fa-car</v-icon>
+                        </v-list-item-avatar>
+                        <v-list-item-content>
+                          <v-list-item-title>
+                            Auto & Transport
+                          </v-list-item-title>
+                          <v-list-item-subtitle
+                            >$ 700 left</v-list-item-subtitle
+                          >
+                        </v-list-item-content>
+                      </v-list-item>
+                    </v-card>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-col>
+            <v-col cols="12" md="4">
+              <v-container>
+                <v-row>
+                  <v-col cols="12" md="12"></v-col>
+                </v-row>
+              </v-container>
+            </v-col>
+          </v-row>
+        </v-app>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+    data: () => ({
+      chartData: [
+        ['Year', 'Sales', 'Expenses', 'Profit'],
+        ['2014', 1000, 400, 200],
+        ['2015', 1170, 460, 250],
+        ['2016', 660, 1120, 300],
+        ['2017', 1030, 540, 350],
+      ],
+      chartOptions: {
+        chart: {
+          title: 'Company Performance',
+          subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+        },
+      },
+    }),
+    components: {},
+    computed: {
+      theme() {
+        return this.$vuetify.theme.dark ? 'dark' : 'light'
+      },
+    },
   }
-}
 </script>
+
+<style scoped>
+  .rounded {
+    border-radius: 30px;
+  }
+  .ligne {
+    border-right: 1px solid #95a5a6;
+  }
+</style>
